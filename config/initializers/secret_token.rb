8c94dddef8769a84b98fadf6655e84efabd00786
pyar6329 <pyar6329@gmail.com>
2014-02-08 20:18:51 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PhotoWander::Application.config.secret_key_base = '77edb2a2bc89d197bd354992544fe298b165ecdaa8ad93e5fcf78fcad01591a10002d65909b2e369135786f76bcd5a270860d8bf37b529a83e27a8c933cb5831'
