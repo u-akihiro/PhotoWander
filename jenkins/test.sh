10d003910b70cf1b83c3aaa482877e65ae4a8fbe
pyar6329 <pyar6329@gmail.com>
2014-02-09 04:29:35 +0900

    database setting

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 bundle install --path .bundle --without production
-bundle exec rake db:migrate
+bundle exec rake db:drop db:create db:migrate
 bundle exec rake db:test:clone
 bundle exec rspec
