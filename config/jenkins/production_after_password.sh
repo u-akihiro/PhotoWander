8aa7f90058e17f370fa0b8f56174894c3995b818
pyar6329 <pyar6329@gmail.com>
2014-02-10 00:55:08 +0900

    bundle place fix

--- config/jenkins/production_after_password.sh
+++ config/jenkins/production_after_password.sh
@@ -5,7 +5,7 @@ ENV=production
 ROOT_DIR="${WORKSPACE}/photowander"
 PID="${ROOT_DIR}/tmp/pids/unicorn.pid"
 CONF="${ROOT_DIR}/config/unicorn.conf.rb"
-CMD="sudo /home/pyar6329/.rbenv/shims/bundle exec ${ROOT_DIR}/.bundle/ruby/2.0.0/bin/unicorn -c ${CONF} -E ${ENV} -D >/dev/null 2>&1 &"
+CMD="sudo /home/pyar6329/.rbenv/versions/2.0.0-p353/bin/bundle exec ${ROOT_DIR}/.bundle/ruby/2.0.0/bin/unicorn -c ${CONF} -E ${ENV} -D >/dev/null 2>&1 &"
 if [ -e $PID ]; then
     echo "stop $NAME"
     sudo kill -QUIT `cat ${PID}`
