74780db63d19036d086a5ef31c0c6d68b80a54bb
pyar6329 <pyar6329@gmail.com>
2014-02-10 01:16:02 +0900

    bundle place fix

--- config/jenkins/production_after_password.sh
+++ config/jenkins/production_after_password.sh
@@ -4,12 +4,13 @@ mkdir -p ${WORKSPACE}/tmp/pids
 mkdir ${WORKSPACE}/tmp/sockets
 mkdir ${WORKSPACE}/tmp/cache
 mkdir ${WORKSPACE}/tmp/sessions
+echo "2.0.0-p353" > ${WORKSPACE}/.ruby-version
 NAME="Unicorn"
 ENV=production
 ROOT_DIR="${WORKSPACE}/photowander"
 PID="${ROOT_DIR}/tmp/pids/unicorn.pid"
 CONF="${ROOT_DIR}/config/unicorn.conf.rb"
-CMD="/home/pyar6329/.rbenv/versions/2.0.0-p353/bin/bundle exec ${ROOT_DIR}/.bundle/ruby/2.0.0/bin/unicorn -c ${CONF} -E ${ENV} -D >/dev/null 2>&1 &"
+CMD="/home/pyar6329/.rbenv/versions/2.0.0-p353/bin/bundle exec ${ROOT_DIR}/.bundle/ruby/2.0.0/bin/unicorn -c ${CONF} -E ${ENV} -D"
 if [ -e $PID ]; then
     echo "stop $NAME"
     kill -QUIT `cat ${PID}`
